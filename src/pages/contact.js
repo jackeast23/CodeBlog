4ec01f3498b4dcf629bd6dd76d85bc668c2e0278
Jack East <23142577+jackeast23@users.noreply.github.com>
2021-02-27 22:56:33 -0500

    init commit

---
+++ src/pages/contact.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ContactPage = () => {
+    return (
+        <div>
+            <h1>Get in contact with me</h1>
+            <h3>Email: jackeast23@gmail.com</h3>
+            <h3>Phone: (613) 985-1523</h3>
+        </div>
+    )
+}
+
+export default ContactPage
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const IndexPage = () => {
+  return (
+    <div>
+        <h1>Hello.</h1>
+        <h2>I'm Jack, a recent Computer Engineering graduate.</h2>
+    </div>
+  )
+}
+
+export default IndexPage

957648635bad1268fc8a180d5e9cd3030656a834
Jack East <23142577+jackeast23@users.noreply.github.com>
2021-02-28 23:41:00 -0500

    added css style page

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 import Header from '../components/header'
 import Footer from '../components/footer'
+import '../styles/index.css'
 
 const Layout = (props) => {
     return (

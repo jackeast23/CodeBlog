d8d8c4b1a9e9c10d32e45dfe9a52715b5a67947b
Jack East <23142577+jackeast23@users.noreply.github.com>
2021-03-01 20:59:52 -0500

    added gatsby sass plugin

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 import Header from '../components/header'
 import Footer from '../components/footer'
-import '../styles/index.css'
+import '../styles/index.scss'
 
 const Layout = (props) => {
     return (

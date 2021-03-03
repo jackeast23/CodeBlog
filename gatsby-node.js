a39feb952e71cfabb28dbc1293e9b24e93b76dbe
Jack East <23142577+jackeast23@users.noreply.github.com>
2021-03-03 02:13:57 -0500

    rendered blog post from cms

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,47 +1,42 @@
 const path = require('path')
 
-module.exports.onCreateNode = ({ node, actions }) => {
-    const { createNodeField } = actions
+// Created this in contentful
+// module.exports.onCreateNode = ({ node, actions }) => {
+//     const { createNodeField } = actions
 
-    if (node.internal.type === 'MarkdownRemark') {
-        const slug = path.basename(node.fileAbsolutePath, '.md')
+//     if (node.internal.type === 'MarkdownRemark') {
+//         const slug = path.basename(node.fileAbsolutePath, '.md')
         
-        createNodeField({
-            node,
-            name: 'slug',
-            value: slug
-        })
-    }
-}
+//         createNodeField({
+//             node,
+//             name: 'slug',
+//             value: slug
+//         })
+//     }
+// }
 
 module.exports.createPages = async ({ graphql, actions }) => {
     const { createPage } = actions
     const blogTemplate = path.resolve('./src/templates/blog.js')
     const res = await graphql(`
         query {
-            allMarkdownRemark {
+            allContentfulBlogPost {
                 edges {
                     node {
-                        fields {
-                            slug
-                        }
+                        slug
                     }
                 }
             }
         }
     `)
 
-    res.data.allMarkdownRemark.edges.forEach((edge) => {
+    res.data.allContentfulBlogPost.edges.forEach((edge) => {
         createPage({
             component: blogTemplate,
-            path: `/blog/${edge.node.fields.slug}`,
+            path: `/blog/${edge.node.slug}`,
             context: {
-                slug: edge.node.fields.slug
+                slug: edge.node.slug
             }
         })
     })
-
-    // 1. Get path to template
-    // 2. Get markdown data
-    // 3. Create new pages
 }
--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,36 +1,30 @@
 import React from 'react'
 import { graphql } from 'gatsby'
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
 
 import Layout from '../components/layout'
 
 export const query = graphql`
-    query (
-        $slug: String!
-    ) {
-        markdownRemark (
-            fields: {
-                slug: {
-                eq: $slug
+    query($slug: String!) {
+        contentfulBlogPost(slug: {eq: $slug}) {
+            title
+            publishedDate(fromNow:true)
+            body {
+                json
             }
-        }
-        ) {
-            frontmatter {
-                title
-                date
-            }
-            html
         }
     }
-  `
+`
 
 const Blog = (props) => {
     return (
         <Layout>
-            <h1>{props.data.markdownRemark.frontmatter.title}</h1>
-            <p>{props.data.markdownRemark.frontmatter.date}</p>
-            <div dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html }}></div>
+            <h1>{props.data.contentfulBlogPost.title}</h1>
+            <p>{props.data.contentfulBlogPost.publishedDate}</p>
+            {documentToReactComponents(props.data.contentfulBlogPost.body.json)}
         </Layout>
     )
 }
 
 export default Blog
+
